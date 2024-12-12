44ca808026e766f2c94054407de9cb6cec4630fe
gimelloc <gislainemcasa@gmail.com>
2024-12-12 14:29:54 -0300

    aula 2

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,26 +1,37 @@
-import { Stack } from "expo-router";
-import { colors } from "@/styles/theme";
-import { useFonts, Rubik_600SemiBold, Rubik_400Regular, Rubik_500Medium, Rubik_700Bold } from "@expo-google-fonts/rubik"
-import { Loading } from "@/components/loading";
+import { Stack } from "expo-router"
+import { colors } from "@/styles/theme"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
-export default function Layout(){
-    const [fontsLoaded] = useFonts({
-        Rubik_600SemiBold, 
-        Rubik_400Regular, 
-        Rubik_500Medium,
-        Rubik_700Bold
-    })
+import {
+  useFonts,
+  Rubik_600SemiBold,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_700Bold,
+} from "@expo-google-fonts/rubik"
 
-    if(!fontsLoaded){
-        return <Loading />
-    }
+import { Loading } from "@/components/loading"
 
-    return <Stack 
-        screenOptions={{ 
-            headerShown: false, 
-            contentStyle: { 
-                backgroundColor: colors.gray[100] 
-            } 
-        }} 
-    />
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Rubik_600SemiBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: colors.gray[100] },
+        }}
+      />
+    </GestureHandlerRootView>
+  )
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://192.168.0.213:3333",
-    timeout: 700,
+    baseURL: "http://192.168.100.38:3333",
+    timeout: 5000,
 })
