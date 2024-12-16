864975235ba394465bd9f13ebdf69c1d6b785d10
gimelloc <gislainemcasa@gmail.com>
2024-12-16 16:18:41 -0300

    inclusao de localizacao

--- src/components/places/index.tsx
+++ src/components/places/index.tsx
@@ -4,6 +4,7 @@ import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet"
 
 import { s } from "./styles"
 import { Place, PlaceProps } from "../place"
+import { router } from "expo-router"
 
 type Props = {
   data: PlaceProps[]
@@ -29,7 +30,7 @@ export function Places({ data }: Props) {
       <BottomSheetFlatList
         data={data}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Place data={item} />}
+        renderItem={({ item }) => <Place data={item} onPress={() => router.navigate(`/market/${item.id}`)} />}
         contentContainerStyle={s.content}
         ListHeaderComponent={() => (
           <Text style={s.title}>Explore locais perto de você</Text>
